2395d84f117883a88acde560337b52b8bcef0f57
sophia wu <sophia6734wu@gmail.com>
2019-06-04 19:47:36 +1000

    Added relations between tables

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,16 @@
 class Project < ApplicationRecord
   has_many :reviews
+  has_many :sentences, through: :reviews
+  has_many :sentence_entities, through: :sentences
+  has_many :entities, through: :sentence_entities
+  # no we could retrieve entities with project instances
+  # e.g. Project.find(2).entities
+
+  # def top_five_entities
+  # end
+  # to get top 5 enities' instances from project instance
+
+  # def worst_five_entities
+  # end
+  # to get worst 5 enities' instances from project instance
 end
