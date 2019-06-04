e362b52a509e89d686b330490ea026b89f53e1ca
sophia wu <sophia6734wu@gmail.com>
2019-06-05 09:35:53 +1000

    edited relations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,8 +2,6 @@ class Entity < ApplicationRecord
   has_many :sentence_entities
   has_many :sentences, through: :sentence_entities
   has_many :reviews, through: :sentences
-  has_many :projects, through: :reviews
-  has_many :users, through: :projects
   # no we could retrieve reviews with entity instances
   # e.g. Entity.find(2).reviews
 
