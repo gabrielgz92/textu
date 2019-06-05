ebeb58a239d11abd63b86aef713fb3f666dd547d
GabrielGZ <ggonzalezzabala@gmail.com>
2019-06-05 14:55:50 +1000

    done project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,11 +13,15 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     @project.user = current_user
     if @project.save
-      parse_csv_into_reviews
       # save project as reviews here when project is created
+      parse_csv_into_reviews
+      # creating sentences
+      create_sentences_from_reviews
+      # creating entities and their relation with sentences
+      create_sentence_entities_and_entities_from_sentences
 
-      redirect_to projects_path
       # just for testing, this needs to redirect to graphs later on
+      redirect_to projects_path
     else
       render :new
     end
@@ -46,6 +50,47 @@ class ProjectsController < ApplicationController
       r.comments = row['comments']
       r.project = @project
       r.save
+    end
+  end
+
+  def create_sentences_from_reviews
+    analyzer = Sentimental.new
+    analyzer.load_defaults
+    analyzer.threshold = 0.1
+
+    @reviews = @project.reviews
+    @reviews.each do |r|
+      r.comments.split(".").each do |sentence|
+        s = Sentence.create(review_id: r.id,
+                            content: sentence)
+        s.update(sentiment_symbol: (analyzer.sentiment s.content),
+                 sentiment_score: (analyzer.score s.content))
+      end
+    end
+  end
+
+  def create_sentence_entities_and_entities_from_sentences
+    @stop_words = []
+    File.open('db/stop_words.txt', "r").reduce([]) do |stop_words, line|
+      @stop_words << line.chomp
+    end
+
+    @sentences = @project.sentences
+    @sentences.each do |sentence|
+      sentence.content.chomp.downcase.split.each do |word|
+        unless @stop_words.include? word
+          if word[-1] == "!" then word.chomp!("!") end
+          entity = Entity.find_by name: word
+          if entity
+            SentenceEntity.create(sentence_id: sentence.id,
+                                  entity_id: entity.id)
+          else
+            new_entity = Entity.create(name: word)
+            SentenceEntity.create(sentence_id: sentence.id,
+                                  entity_id: new_entity.id)
+          end
+        end
+      end
     end
   end
 end
