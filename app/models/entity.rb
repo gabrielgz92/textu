2c4bc36145f5d804c1e59c43dd04cb1a3de41efa
tom conway <thomconway94@gmail.com>
2019-06-06 15:03:15 +1000

    working method

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -14,4 +14,38 @@ class Entity < ApplicationRecord
   def occurrences
     sentence_entities.count
   end
+
+  # def self.sentiment_over_time
+  #   # average_sentiment, date_created
+  #   sentiment_scores = []
+  #   entity = Entity.find_by(name: "location")
+  #   reviews = entity.reviews
+  #   reviews.each do |review|
+  #     # compute sentiment score from occurences of entity within sentences
+  #     sentences = review.sentences
+  #     # pluck out sentences with the entity inside them
+  #     occurences = sentences.select { |sentence| sentence.content.include? "location" }
+  #     average_sentiment = occurences.pluck(:sentiment_score).reduce(&:+) / occurences.count
+  #     sentiment_scores << { date_created: review.date, average_sentiment: average_sentiment }
+  #   # sentiment_scores << [review.date, average_sentiment]
+  #   binding.pry
+  #     sentiment_scores
+  #   end
+  # end
+
+  def sentiment_over_time
+    # average_sentiment, date_created
+    sentiment_scores = []
+    reviews = self.reviews
+    reviews.each do |review|
+      # compute sentiment score from occurences of entity within sentences
+      sentences = review.sentences
+      # pluck out sentences with the entity inside them
+      occurences = sentences.select { |sentence| sentence.content.include? name }
+      average_sentiment = occurences.pluck(:sentiment_score).reduce(&:+) / occurences.count
+      sentiment_scores << { date_created: review.date, average_sentiment: average_sentiment }
+    # sentiment_scores << [review.date, average_sentiment]
+    end
+    sentiment_scores
+  end
 end
