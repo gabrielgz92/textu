6757581c324efed5185778884b74bf0c57d2803d
tom conway <thomconway94@gmail.com>
2019-06-10 17:01:48 +1000

    fixed

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,9 +1,11 @@
 class EntitiesController < ApplicationController
-  before_action :set_project, only: %i[show entites_data]
+  before_action :set_project, only: %i[index show entites_data]
   before_action :set_entities, only: %i[index reviews_for_entity]
 
   def index
     @entity = Entity.find_by(name: params[:entity])
+    @first_word_score = first_word_score
+    @last_word_score = last_word_score
     render layout: 'tour'
   end
 
@@ -35,4 +37,18 @@ class EntitiesController < ApplicationController
   def entities_params
     params.permit(:entity)
   end
+
+  def first_word_score
+    first_word = @entity.reviews.map{|x| [x.date, x.id] }.sort.first
+    byebug
+    test_re = @project.reviews.find(first_word[1]).sentences.select { |sentence| sentence.content.include? @entity.name }
+    test_re[0].sentiment_score
+  end
+
+  def last_word_score
+    last_word = @entity.reviews.map{|x| [x.date, x.id] }.sort.second
+    Review.find(last_word[1]).sentences.first.sentiment_score
+  end
 end
+
+
