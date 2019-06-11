7f432081b73d7b7764e83233b2428271e92e09a6
GabrielGZ <ggonzalezzabala@gmail.com>
2019-06-11 13:47:15 +1000

    finished conclusion view

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -43,19 +43,19 @@ class Entity < ApplicationRecord
   end
 
   def self.top_highest_sentiment_for_project(project_id)
-    Project.find(project_id).entities.sort_by(&:avg_sentiment).reverse!.map { |x| [x.name, x.occurrences] }.first(2)
+    Project.find(project_id).entities.sort_by(&:avg_sentiment).reverse!.map { |x| [x.name, x.occurrences] }.first(5)
   end
 
   def self.top_highest_sentiment_with_avgs_for_project(project_id)
-    Project.find(project_id).entities.sort_by(&:avg_sentiment).reverse!.map { |x| [x.name] }.first(2)
+    Project.find(project_id).entities.sort_by(&:avg_sentiment).reverse!.map { |x| [x.name] }.first(5)
   end
 
   def self.top_lowest_sentiment_for_project(project_id)
-    Project.find(project_id).entities.sort_by(&:avg_sentiment).map { |x| [x.name, x.occurrences] }.first(2)
+    Project.find(project_id).entities.sort_by(&:avg_sentiment).map { |x| [x.name, x.occurrences] }.first(5)
   end
 
   def self.top_lowest_sentiment_with_avgs_for_project(project_id)
-    Project.find(project_id).entities.sort_by(&:avg_sentiment).map { |x| [x.name] }.first(2)
+    Project.find(project_id).entities.sort_by(&:avg_sentiment).map { |x| [x.name] }.first(5)
   end
 end
 
