27c741e2f5116d1cc23978318c49ad7f240a8803
tom conway <thomconway94@gmail.com>
2019-06-12 16:08:27 +1000

    sticky sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
 import "bootstrap";
-require("chartkick")
-require("chart.js")
+require("chartkick");
+require("chart.js");
+const Sticky = require("sticky-js");
+
+// Sticky selector for the word list
+const sticky = new Sticky('.js-sticky');
